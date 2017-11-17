824841e9d78dde04ec5086b4e64e84255046676f
ShabnamSheikhha <shablandelina@gmail.com>
2017-11-18 00:11:09 +0330

    adding the alien class

---
+++ src/Alien.java
@@ -0,0 +1,81 @@
+public class Alien {
+    private String name;
+    private int energy;
+    private int speed;
+    private int shootingSpeed;
+    private int strength;
+    private boolean canFly;
+
+    public Alien(String name) {
+        this.name = name;
+        switch (name){
+            case "Albertonion":
+                this.energy = 250;
+                this.speed = 8;
+                this.shootingSpeed = 5;
+                this.strength = 7;
+                this.canFly = false;
+                break;
+            case "Algwasonion":
+                this.energy = 150;
+                this.speed = 4;
+                this.shootingSpeed = 10;
+                this.strength = 25;
+                this.canFly = false;
+                break;
+            case "Activionion":
+                this.energy = 400;
+                this.speed = 2;
+                this.shootingSpeed = 2;
+                this.strength = 40;
+                this.canFly = false;
+                break;
+            case "Aironion":
+                this.energy = 200;
+                this.speed = 5;
+                this.shootingSpeed = 5;
+                this.strength = 20;
+                this.canFly = true;
+                break;
+        }
+    }
+
+    public Alien(String name, int energy, int initialSpeed, int shootingSpeed, int strength, boolean canFly) { //custom constructor?!
+        this.name = name;
+        this.energy = energy;
+        this.speed = initialSpeed;
+        this.shootingSpeed = shootingSpeed;
+        this.strength = strength;
+        this.canFly = canFly;
+    }
+
+    public boolean isDead(){
+        return energy <= 0;
+    }
+
+    public void fire(Warrior warrior){}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getShootingSpeed() {
+        return shootingSpeed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public boolean isCanFly() {
+        return canFly;
+    }
+}
