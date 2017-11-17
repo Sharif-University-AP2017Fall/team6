889ed5956fc9d94fb24be6ecba493ad826ec618c
ShabnamSheikhha <shablandelina@gmail.com>
2017-11-18 00:42:22 +0330

    V0.1

---
+++ src/Dimension.java
@@ -0,0 +1,9 @@
+public class Dimension {
+    private int x;
+    private int y;
+
+    public Dimension(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/Wormhole.java
@@ -0,0 +1,30 @@
+public class Wormhole {
+    private int number;
+    private int leadsTo;
+    private Dimension dimension;
+
+    public Wormhole(int number, int leadsTo, Dimension dimension) {
+        this.number = number;
+        this.leadsTo = leadsTo;
+        this.dimension = dimension;
+    }
+
+    public void moveThrough(Moveable moveable){
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getLeadsTo() {
+        return leadsTo;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+}
