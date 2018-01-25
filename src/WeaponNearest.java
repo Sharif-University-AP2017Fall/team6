5f9d2c3e8fc567bab6432616298083eeda485753
Tara <tb6731@yahoo.com>
2018-01-26 00:13:39 +0330

    Weapon killing aliens with bullet that cannot be seen

--- src/WeaponNearest.java
+++ src/WeaponNearest.java
@@ -1,13 +1,22 @@
 
+
+
 import java.util.ArrayList;
 
 import java.util.List;
+import javafx.animation.FadeTransition;
+
+import javafx.animation.PathTransition;
+import javafx.animation.PathTransition.OrientationType;
 
 import javafx.scene.image.Image;
 
 import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.StackPane;
+
+import javafx.scene.shape.*;
+import javafx.util.Duration;
 
 
 /*
@@ -167,14 +176,20 @@ class WeaponNearestView extends WeaponView {
      
      public int index;
 
-
+     public int indB;
+     
+     public ImageView bul;
+     
+     private String nameW; 
+     
      public WeaponNearestView(String name, Dimension dim_){
          
          System.out.println("setting view for " + name);
          this.pic = new ImageView[8];
          index = 5;
          dim=dim_;
-
+         nameW=name;
+         
          String address = "res/weapons/" + name +"/";
          pic[4] = new ImageView(new Image(getClass()
                  .getResource(address + "5.png").toExternalForm()));
@@ -194,6 +209,17 @@ class WeaponNearestView extends WeaponView {
             pic[i].setVisible(false);
 
          }
+         
+
+            bul = new ImageView(new Image(getClass()
+                    .getResource(address+"10.png").toExternalForm()));
+            bul.setFitWidth(8);
+            bul.setFitHeight(8);
+            bul.setVisible(false);
+
+         
+         
+         
          pic[4].setVisible(true);
          getChildren().addAll(pic[0],
                  pic[1],
@@ -202,8 +228,9 @@ class WeaponNearestView extends WeaponView {
                  pic[4],
                  pic[5],
                  pic[6],
-                 pic[7]
-                 );
+                 pic[7],
+                 bul
+                );
          
          setTranslateX(dim_.getX());
          setTranslateY(dim_.getY());
@@ -224,6 +251,8 @@ class WeaponNearestView extends WeaponView {
          
      }
 
+     
+     
     @Override
      public void setPic(int i){
          
@@ -239,80 +268,124 @@ class WeaponNearestView extends WeaponView {
          double deltaX=Dimension.deltaX(this.dim, prey);
          double deltaY=Dimension.deltaY(this.dim, prey);
          
-         if (deltaX==0){
-             if(deltaY>0){
-                 
-                 setPic(1);
-                 
-             }
-             else{
-                 
-                 setPic(6);  
-                 
-             }  
-             return;
-         }
+         changePic(deltaX,deltaY);
          
-         double slope=Math.atan(deltaY/deltaX);
+         ImageView bullet=new ImageView(new Image(getClass()
+                    .getResource("res/weapons/" + nameW +"/10.png").toExternalForm()));;
          
-         if(deltaX>0){
-             if(slope<0.4 && slope>-0.4)
-                {
-                setPic(0);
-                return;
-                }
-             if(slope>0.4 && slope<1.2)
-                {
-                setPic(1);
-                return;
-                }
-             if(slope<-0.4 && slope>-1.2)
-                {
-                setPic(7);
-                return;
-                }
-             if(slope<-1.2)
-                {
-                setPic(6);
-                return;
-                }
-             if(slope>1.2)
-                {
-                setPic(2);
-                return;
-                }
-         }
-         else{
-             if(slope<0.4 && slope>-0.4)
-                {
-                setPic(4);
-                return;
-                }
-             if(slope>0.4 && slope<1.2)
-                {
-                setPic(3);
-                return;
-                }
-             if(slope<-0.4 && slope>-1.2)
-                {
-                setPic(5);
-                return;
-                }
-             if(slope<-1.2)
-                {
-                setPic(6);
-                return;
-                }
-             if(slope>1.2)
-                {
-                setPic(2);
-                return;
-                } 
-         }
+                    
+                    
+         Path path = new Path();
+         path.getElements().add (new MoveTo (dim.getX(), dim.getY()));
          
+         LineTo lineTo=new LineTo(deltaX,deltaY);
+         path.getElements().add (lineTo);
+         
+         PathTransition pathTransition=new PathTransition();
+         pathTransition.setDuration(Duration.millis(100));
+         pathTransition.setNode(bullet);
+         pathTransition.setPath(path);
+         pathTransition.setOrientation(OrientationType.ORTHOGONAL_TO_TANGENT);
+         pathTransition.setCycleCount(2);
+         pathTransition.setAutoReverse(true);
 
+         pathTransition.play();
+         
+          
+         FadeTransition ft = new FadeTransition(Duration.millis(100), bullet);
+         ft.setFromValue(1.0);
+         ft.setToValue(0);
+         ft.setCycleCount(1);
+         ft.setAutoReverse(false);
+
+         ft.play();
+         
          
      }
+     
+     
+     
+     public void changePic(double deltaX,double deltaY){
+     
+         
+            if (deltaX==0){
+                    if(deltaY>0){
+
+                        setPic(1);
+
+                    }
+                    else{
+
+                        setPic(6);  
+
+                    }  
+                    return;
+                }
+
+                double slope=Math.atan(deltaY/deltaX);
+
+                if(deltaX>0){
+                    if(slope<0.4 && slope>-0.4)
+                       {
+                       setPic(0);
+                       return;
+                       }
+                    if(slope>0.4 && slope<1.2)
+                       {
+                       setPic(1);
+                       return;
+                       }
+                    if(slope<-0.4 && slope>-1.2)
+                       {
+                       setPic(7);
+                       return;
+                       }
+                    if(slope<-1.2)
+                       {
+                       setPic(6);
+                       return;
+                       }
+                    if(slope>1.2)
+                       {
+                       setPic(2);
+                       return;
+                       }
+                }
+                else{
+                    if(slope<0.4 && slope>-0.4)
+                       {
+                       setPic(4);
+                       return;
+                       }
+                    if(slope>0.4 && slope<1.2)
+                       {
+                       setPic(3);
+                       return;
+                       }
+                    if(slope<-0.4 && slope>-1.2)
+                       {
+                       setPic(5);
+                       return;
+                       }
+                    if(slope<-1.2)
+                       {
+                       setPic(6);
+                       return;
+                       }
+                    if(slope>1.2)
+                       {
+                       setPic(2);
+                       return;
+                       } 
+                }
+
+
+     
+     
+     
+     
+     
+     } 
      
      
      
