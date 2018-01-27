023d0fd6dba30c8f87ca15c2d324a1f6254cdd6d
Tara <tb6731@yahoo.com>
2018-01-27 23:58:17 +0330

    server-client

---
+++ src/Client.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Scanner screenScanner=new Scanner(System.in);
+        System.err.println("Try to connect to server");
+        Socket socket;// = new Socket("localhost", 9999);
+        System.err.println("Connected to the server");
+        Formatter formatter;// = new Formatter(socket.getOutputStream());
+        Scanner scanner;
+        System.err.println("sending message");
+        
+        
+        for (int i=0;i<10;i++){
+            socket = new Socket("localhost", 9999);
+            formatter= new Formatter(socket.getOutputStream());
+            formatter.format(screenScanner.nextLine()+"\n");
+            formatter.flush();
+            System.err.println("Message sent");
+            System.err.println("Waiting for Answer");
+            scanner = new Scanner(socket.getInputStream());
+            System.out.println("Server: "+scanner.nextLine());
+            scanner.close();
+            formatter.close();
+            socket.close();
+        }
+        
+        //socket.close();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.err.println("Listening to port 9999");
+        Scanner screenScanner=new Scanner(System.in);
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.err.println("Waiting for client to connect");
+        Socket socket;// = serverSocket.accept();
+        System.err.println("Connected to the client");
+        Formatter formatter ;
+        Scanner scanner ;
+        
+        
+        for (int i=0;i<10;i++){
+        socket = serverSocket.accept();
+        scanner = new Scanner(socket.getInputStream());
+        String arg = scanner.nextLine();
+        System.out.println("Client: "+ arg);
+        formatter = new Formatter(socket.getOutputStream());
+        formatter.format(screenScanner.nextLine()+"\n");
+        formatter.flush();
+        formatter.close();
+        socket.close();
+        }
+        
+        //socket.close();
+    }
+}
